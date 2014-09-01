1f78f7220f9b2167914403bb26afa8294e057b7f
jas0ndiam0nd <jason.diamond@sutherlandglobal.com>
2014-09-01 15:39:22 -0400

    Datasources package, holds the spots on the fs with our db connection config files.

---
+++ src/hughes/datasources/DatabaseConfigs.java
@@ -0,0 +1,7 @@
+package hughes.datasources;
+
+public final class DatabaseConfigs 
+{
+	public final static String PRIVATE_LABEL_PROD_DB = "/opt/tomcat/apache-tomee-plus/webapps/Hughes/WEB-INF/lib/conf/database/rocjfsdbs27.properties";
+	public final static String PRIVATE_LABEL_DEV_DB = "/opt/tomcat/apache-tomee-plus/webapps/Hughes/WEB-INF/lib/conf/database/rocjfsdev18.properties";
+}
---
+++ src/hughes/datasources/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author jason
+ *
+ */
+package hughes.datasources;
